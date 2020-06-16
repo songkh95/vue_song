9f174d78c05a7f2266c08f4444280248fa16979c
songkh95 <41109449+songbass@users.noreply.github.com>
2020-06-16 16:50:17 +0900

    06/16-2

--- v_capstone/src/components/Products.vue
+++ v_capstone/src/components/Products.vue
@@ -1,49 +1,66 @@
 <template>
   <div id="Products">
     <!-- 메뉴 -->
-      <header>
-          <Menu />
-      </header>
-      <section class="section_top">
-        <!-- 상품 이미지 -->
-          <div class="products_img">
-              <img :src="require('../assets/product_img/' + $store.state.product_content.name + '.png')" alt="PPL 이미지" />
+    <header>
+      <Menu />
+    </header>
+
+    <section class="section_top">
+      <div>
+        <img :src="require('../assets/product_img/' + $store.state.product_content.name + '.png')" alt="PPL 이미지" />
+      </div>
+      <div class="Products_content">
+        <h2>{{$store.state.product_content.color == "color" ?  "칼라복합기" : "흑백복합기"}}</h2>
+        <h1>{{$store.state.product_content.name}}</h1>
+        <hr>
+
+        <h3 class="Products_content_subheading">{{$store.state.product_content.subheading}}</h3>
+        <p>
+          {{$store.state.product_content.content}}
+        </p>
+          <div class="products_content_Performance">
+            <div class="products_content_border">
+              <h3><strong>분당 프린트 속도</strong></h3>
+              <h4>{{$store.state.product_content.print_speed}}매 / {{$store.state.product_content.print_speed_level}} 급
+              </h4>
+            </div>
+            <div class="products_content_border">
+              <h3> <strong>분당 스캔 속도</strong> </h3>
+              <h4>{{$store.state.product_content.scan_speed}}매</h4>
+            </div>
+            <div class="products_content_border">
+              <h3><strong>출력 해상도</strong></h3>
+              <h4>{{$store.state.product_content.dpi}}</h4>
+            </div>
+            <div>
+              <h3><strong>팩스</strong></h3>
+              <h4>{{$store.state.product_content.fax_kit}}</h4>
+            </div>
+          </div><br>
+          <div class="products_content_support">
+            {{$store.state.product_content.support}}<br>
+          
+            배송안내 : 무료배송 <br>
+
+            금액: {{$store.state.product_content.sale}} <br>
           </div>
-          <!-- 상품 내용 -->
-          <div class="Products_content">
-                <h1>{{$store.state.product_content.name}}</h1>
-              
-              <p>
-                사용자의 비지니스 확대는 새로운 ApeosPort 시리즈의 강력한 성능을 발휘하게 합니다.<br>
-                전반적으로 생산성 향상이 필요한 환경에 대해 클라우드 서비스와 연동시킨 기기들의 통합,<br>
-                스마트폰과의 원활한 연결과 같이 최적의 수준으로 확장이 가능해집니다. <br><br>
+          
+          <button class="btn_estimate"> 견적 문의 </button>
+      </div>
+    </section>
+    <!-- 참고 -->
+    <section class="section_mid">
+      asd<br><br><br><br><br>
+    </section>
+    <!-- 제품 상세설명 -->
+    <section class="section_bottom">
+      <img :src="require('../assets/product_detail/' + $store.state.product_content.name +'.jpg')" alt="상품 상세내용 이미지" />
+    </section>
+    <!-- 밑 -->
+    <footer>
 
-                분당 프린트 속도: {{$store.state.product_content.print_speed}}매 / {{$store.state.product_content.print_speed_level}}등급<br>
-                분당 스캔 속도: {{$store.state.product_content.scan_speed}}매 <br>
-                프린트 해상도:  {{$store.state.product_content.dpi}}<br><br>
-
-                무선랜 지원 | 모바일 지원 | 클라우드 인터랙티브 <br>
-                배송안내 : 무료배송 <br><br>
-
-                구입 금액: {{$store.state.product_content.sale}} <br><br>
-                
-                <button> 견적 문의 </button>
-              </p>
-          </div>
-      </section>
-      <!-- 참고 -->
-      <section class="section_mid">
-          asd<br><br><br><br><br>
-      </section>
-      <!-- 제품 상세설명 -->
-      <section class="section_bottom">
-        <!-- <img :src="require(`../assets/${$store.state.product_content.name}_detail.jpg`)" alt="상품 이미지" /> -->
-      </section>
-      <!-- 밑 -->
-      <footer>
-
-            <br><br><br><br><br>
-      </footer>
+      <br><br><br><br><br>
+    </footer>
   </div>
 </template>
 
@@ -81,36 +98,113 @@ export default {
 
 <style scoped>
 header{
-    background: #ffffff;
+   position: relative;
+   top:0;
 }
 .section_top{
-    background: #ffffff;
-}
-.products_img{
-    position: relative;
-    float: left;
-    width: 50%;
-    background: #ffffff;
+  position: relative;
+  left:21vw;
+  height: 30vh;
+  margin-top: 10vh;
+  margin-bottom: 50vh;
 }
 
-.products_img img{
+.section_top img{
     position: relative;
-    margin-left: 40%;
-    width: 30%;
-    background: #ffffff;
+    float: left;
+    width: 18vw;
 }
 
 .Products_content{
     position: relative;
     float: left;
-    width: 50%;
-    background: #ffffff;
+    left: 5vw;
+    top: 5vh;
+    width: 42%;
 }
-.section_mid{
-    position: relative;
-    float: left;
-    width: 100%;
-    background: #145463;
+.Products_content h1{
+  margin-bottom: -20px;
+  font-size: 60px;
+  font-weight: 300;
+}
+.Products_content h2{
+  font-size: 2.5vh;
+  color: #424242;
+  font-weight: 300;
+}
+
+.Products_content hr{
+  width: 66%;
+  margin-top: 2.5vh;
+}
+.Products_content_subheading{
+  position: relative;
+  margin-top: 2vh;
+}
+.Products_content h3{
+ position: relative;
+ 
+}
+
+.Products_content p{
+  position: relative;
+  margin-top: 2vh;
+  width: 500px;
+}
+
+.products_content_border{
+  border-right: 1px solid #9c9c9c;
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+.products_content_Performance strong{
+  color: rgb(29, 51, 93);
+}
+.products_content_Performance h4{
+  color: rgb(0, 0, 0);
+}
+
+.products_content_Performance{
+  position: relative;
+  top: 5vh;
+  height: 13vh;
+}
+.products_content_Performance h3{
+  margin-bottom: 10px;
+}
+
+.products_content_Performance div{
+  position: relative;
+  float: left;
+  text-align: center;
+  padding-right: 10px;
+}
+.products_content_border{
+  position: relative;
+  border-right: 1px solid #9c9c9c;
+  margin-right: 10px;
+  margin-bottom: 10px;
+}
+.products_content_support{
+  line-height: 30px;
+  position: relative;
+  float: left;
+  width: 100%;
+}
+.btn_estimate{
+  position: relative;
+  float: left;
+  top: 5vh;
+  border: none;
+  background: rgb(29, 51, 93);
+  /* padding-bottom: 2px;
+  padding-left: 3px; */
+  width:15vw;
+  height: 6vh;
+  font-size: 15px;
+  color: #fff;
+  font-weight: 100;
+  border-radius: 0px;
 }
 .section_bottom{
     position: relative;
@@ -121,6 +215,12 @@ header{
 .section_bottom img{
     margin-left: 20%;
     width: 50%;
+}
+.section_mid{
+    position: relative;
+    float: left;
+    width: 100%;
+    background: #145463;
 }
 footer{
     position: relative;
